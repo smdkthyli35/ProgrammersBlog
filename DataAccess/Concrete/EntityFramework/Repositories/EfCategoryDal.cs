96b2e8cf75eac671e657a80759757154002e195d
zeljkosamed <smdkthyli@gmail.com>
2022-01-09 22:48:32 +0300

    EfEntityRepositoryBase'den türeyen repository sınıfları oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/Repositories/EfCategoryDal.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.Concrete.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Repositories
+{
+    public class EfCategoryDal : EfEntityRepositoryBase<Category>, ICategoryDal
+    {
+        public EfCategoryDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
