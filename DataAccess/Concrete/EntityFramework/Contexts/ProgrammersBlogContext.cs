3146d9b044eb4c17dac7addd3cd1b6a4b2abd290
zeljkosamed <smdkthyli@gmail.com>
2022-01-09 23:08:38 +0300

    DbContext sınıfı oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class ProgrammersBlogContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ProgrammersBlog;Trusted_Connection=True;ConnectTimeout=30;MultipleActiveResultSets=True;");
+        }
+    }
+}
