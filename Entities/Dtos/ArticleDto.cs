27cb5516386ca7e6590123b58ed7a2e18ddc692d
zeljkosamed <smdkthyli@gmail.com>
2022-01-10 23:16:04 +0300

    IArticleService ve Dtoları oluşturuldu.

---
+++ Entities/Dtos/ArticleDto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using Core.Utilities.Results.ComplexTypes;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class ArticleDto : DtoGetBase
+    {
+        public Article Article { get; set; }
+    }
+}
