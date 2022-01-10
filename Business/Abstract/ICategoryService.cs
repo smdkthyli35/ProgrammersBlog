361303ddc49969050c4bb636fe2175685a35b435
zeljkosamed <smdkthyli@gmail.com>
2022-01-10 17:27:39 +0300

    ICategoryService ve Dtoları oluşturuldu.

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int categoryId);
+        Task<IDataResult<IList<Category>>> GetAll();
+        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int categoryId);
+        Task<IResult> HardDelete(int categoryId);
+    }
+}
