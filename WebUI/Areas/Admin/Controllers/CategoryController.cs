fbfd42eca31e32f70235c9a77155fe3b70965b04
zeljkosamed <smdkthyli@gmail.com>
2022-01-12 20:00:05 +0300

    Kategoriler datatable'da listelendi.

---
+++ WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+    }
+}
