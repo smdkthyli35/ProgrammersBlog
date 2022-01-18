c49254221739d2886fe58aed9af1424aae5ecb9a
zeljkosamed <smdkthyli@gmail.com>
2022-01-18 20:23:18 +0300

    Kategori güncelleme işlemi oluşturuldu.

--- WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -64,12 +64,35 @@ namespace WebUI.Areas.Admin.Controllers
             var result = await _categoryService.GetCategoryUpdateDto(categoryId);
             if (result.ResultStatus == ResultStatus.Success)
             {
-                return PartialView("_CategoryUpdatePartial",result.Data);
+                return PartialView("_CategoryUpdatePartial", result.Data);
             }
             else
             {
                 return NotFound();
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(CategoryUpdateDto categoryUpdateDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _categoryService.Update(categoryUpdateDto, "Samed Kütahyalı");
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var categoryUpdateAjaxModel = JsonSerializer.Serialize(new CategoryUpdateAjaxViewModel
+                    {
+                        CategoryDto = result.Data,
+                        CategoryUpdatePartial = await this.RenderViewToStringAsync("_CategoryUpdatePartial", categoryUpdateDto)
+                    });
+                    return Json(categoryUpdateAjaxModel);
+                }
+            }
+            var categoryUpdateAjaxErrorModel = JsonSerializer.Serialize(new CategoryUpdateAjaxViewModel
+            {
+                CategoryUpdatePartial = await this.RenderViewToStringAsync("_CategoryUpdatePartial", categoryUpdateDto)
+            });
+            return Json(categoryUpdateAjaxErrorModel);
         }
 
         public async Task<JsonResult> GetAllCategories()
---
+++ WebUI/Areas/Admin/Models/CategoryUpdateAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.Models
+{
+    public class CategoryUpdateAjaxViewModel
+    {
+        public CategoryUpdateDto CategoryUpdateDto { get; set; }
+        public string CategoryUpdatePartial { get; set; }
+        public CategoryDto CategoryDto { get; set; }
+    }
+}
