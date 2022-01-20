93396c88c4856b06307c06744a403a8492239e53
zeljkosamed <smdkthyli@gmail.com>
2022-01-20 22:38:53 +0300

    Kullanıcı ekleme işlemi frontend tarafı tamamlandı.

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using WebUI.AutoMapper.Profiles;
 
 namespace WebUI
 {
@@ -25,7 +26,7 @@ namespace WebUI
                 opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
             });
             services.AddSession();
-            services.AddAutoMapper(typeof(CategoryProfile), typeof(ArticleProfile));
+            services.AddAutoMapper(typeof(CategoryProfile), typeof(ArticleProfile),typeof(UserProfile));
             services.LoadMyServices();
             services.ConfigureApplicationCookie(options =>
             {
