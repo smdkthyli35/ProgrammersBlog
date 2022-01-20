c435390b7cce1f09c6f83f5b92f29afa531689f9
zeljkosamed <smdkthyli@gmail.com>
2022-01-20 21:23:36 +0300

    DateTime Extension ve ImageUpload metodu ile resim yükleme tamamlandı.

--- WebUI/Areas/Admin/Controllers/UserController.cs
+++ WebUI/Areas/Admin/Controllers/UserController.cs
@@ -1,11 +1,14 @@
-﻿using Core.Utilities.Results.ComplexTypes;
+﻿using Business.Extensions;
+using Core.Utilities.Results.ComplexTypes;
 using Entities.Concrete;
 using Entities.Dtos;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,10 +18,12 @@ namespace WebUI.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly IWebHostEnvironment _env;
 
-        public UserController(UserManager<User> userManager)
+        public UserController(UserManager<User> userManager, IWebHostEnvironment env)
         {
             _userManager = userManager;
+            _env = env;
         }
 
         public async Task<IActionResult> Index()
@@ -35,6 +40,21 @@ namespace WebUI.Areas.Admin.Controllers
         public IActionResult Add()
         {
             return PartialView("_UserAddPartial");
+        }
+
+        public async Task<string> ImageUpload(UserAddDto userAddDto)
+        {
+            string wwwroot = _env.WebRootPath;
+            //string fileName2 = Path.GetFileNameWithoutExtension(userAddDto.Picture.FileName);
+            string fileExtension = Path.GetExtension(userAddDto.Picture.FileName);
+            DateTime dateTime = DateTime.Now;
+            string fileName = $"{userAddDto.UserName}_{dateTime.FullDateAndTimeStringWithUnderscore()}{fileExtension}";
+            var path = Path.Combine($"{wwwroot}/img", fileName);
+            await using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await userAddDto.Picture.CopyToAsync(stream);
+            }
+            return fileName;
         }
     }
 }
