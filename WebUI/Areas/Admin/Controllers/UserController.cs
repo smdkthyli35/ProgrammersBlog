0cf54be393a4b73f1d021d41a10ffdcad31bdf97
zeljkosamed <smdkthyli@gmail.com>
2022-01-23 14:13:56 +0300

    UserLoginDto, UserLoginLayout ve UserLogin sayfaları oluşturuldu.

--- WebUI/Areas/Admin/Controllers/UserController.cs
+++ WebUI/Areas/Admin/Controllers/UserController.cs
@@ -45,6 +45,12 @@ namespace WebUI.Areas.Admin.Controllers
         }
 
         [HttpGet]
+        public IActionResult UserLogin()
+        {
+            return View();
+        }
+
+        [HttpGet]
         public async Task<JsonResult> GetAllUsers()
         {
             var users = await _userManager.Users.ToListAsync();
