fabd741a6c6b22df51501758f929f5240229606e
zeljkosamed <smdkthyli@gmail.com>
2022-01-24 14:14:14 +0300

    Admin menüsü dinamik hale getirildi.

---
+++ WebUI/Areas/Admin/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.Models
+{
+    public class UserWithRolesViewModel
+    {
+        public User User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
