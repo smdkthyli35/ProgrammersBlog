0d5cfda52462323069d5d61e29f76e618420e6e8
zeljkosamed <smdkthyli@gmail.com>
2022-01-29 13:47:30 +0300

    jQuery UI kütüphanesi ve DatePicker eklentisi eklendi.

--- WebUI/wwwroot/AdminLTE/js/articleAdd.js
+++ WebUI/wwwroot/AdminLTE/js/articleAdd.js
@@ -35,4 +35,33 @@
     });
 
     //Select2
+
+    //jQuery UI - Datepicker
+
+    $(function () {
+        $("#datepicker").datepicker({
+            closeText: "kapat",
+            prevText: "&#x3C;geri",
+            nextText: "ileri&#x3e",
+            currentText: "bugün",
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+                "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz",
+                "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+            dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+            dayNamesShort: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            dayNamesMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            weekHeader: "Hf",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "",
+            duration: 1000,
+            minDate: -3,
+            maxDate: +3
+        });
+    });
+
+    //jQuery UI - Datepicker
 });
