8af62cdaebced6a6e784c820a430aeb7ff98e828
zeljkosamed <smdkthyli@gmail.com>
2022-03-14 16:42:11 +0300

    RoleAssignDto ve UserRoleAssignDto sınıfları oluşturuldu.

---
+++ Entities/Dtos/RoleAssignDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class RoleAssignDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool HasRole { get; set; }
+    }
+}
