e5e23609eaa68a7a138b829bf3bbcb902ddb416d
zeljkosamed <smdkthyli@gmail.com>
2022-03-21 18:00:06 +0300

    Makale eklerken select listin verdiği hata mesajı türkçeleştirildi.

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -29,7 +29,10 @@ namespace WebUI
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews().AddRazorRuntimeCompilation().AddJsonOptions(opt =>
+            services.AddControllersWithViews(options =>
+            {
+                options.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(value => "Bu alan boþ geçilmemelidir.");
+            }).AddRazorRuntimeCompilation().AddJsonOptions(opt =>
             {
                 opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                 opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
