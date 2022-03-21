b3e8a881b9c416c9d96d84332938a25177e6c9ca
zeljkosamed <smdkthyli@gmail.com>
2022-03-21 17:09:31 +0300

    _RoleAssignPartial sayfası oluşturuldu.

--- WebUI/wwwroot/AdminLTE/js/userIndex.js
+++ WebUI/wwwroot/AdminLTE/js/userIndex.js
@@ -72,37 +72,209 @@
                 }
             }
         ],
-        language: {
-            "sDecimal": ",",
-            "sEmptyTable": "Tabloda herhangi bir veri mevcut değil",
-            "sInfo": "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
-            "sInfoEmpty": "Kayıt yok",
-            "sInfoFiltered": "(_MAX_ kayıt içerisinden bulunan)",
-            "sInfoPostFix": "",
-            "sInfoThousands": ".",
-            "sLengthMenu": "Sayfada _MENU_ kayıt göster",
-            "sLoadingRecords": "Yükleniyor...",
-            "sProcessing": "İşleniyor...",
-            "sSearch": "Ara:",
-            "sZeroRecords": "Eşleşen kayıt bulunamadı",
-            "oPaginate": {
-                "sFirst": "İlk",
-                "sLast": "Son",
-                "sNext": "Sonraki",
-                "sPrevious": "Önceki"
-            },
-            "oAria": {
-                "sSortAscending": ": artan sütun sıralamasını aktifleştir",
-                "sSortDescending": ": azalan sütun sıralamasını aktifleştir"
-            },
-            "select": {
-                "rows": {
-                    "_": "%d kayıt seçildi",
-                    "0": "",
-                    "1": "1 kayıt seçildi"
-                }
-            }
-        }
+       language: {
+           "emptyTable": "Tabloda herhangi bir veri mevcut değil",
+           "info": "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
+           "infoEmpty": "Kayıt yok",
+           "infoFiltered": "(_MAX_ kayıt içerisinden bulunan)",
+           "infoThousands": ".",
+           "lengthMenu": "Sayfada _MENU_ kayıt göster",
+           "loadingRecords": "Yükleniyor...",
+           "processing": "İşleniyor...",
+           "search": "Ara:",
+           "zeroRecords": "Eşleşen kayıt bulunamadı",
+           "paginate": {
+               "first": "İlk",
+               "last": "Son",
+               "next": "Sonraki",
+               "previous": "Önceki"
+           },
+           "aria": {
+               "sortAscending": ": artan sütun sıralamasını aktifleştir",
+               "sortDescending": ": azalan sütun sıralamasını aktifleştir"
+           },
+           "select": {
+               "rows": {
+                   "_": "%d kayıt seçildi",
+                   "1": "1 kayıt seçildi"
+               },
+               "cells": {
+                   "1": "1 hücre seçildi",
+                   "_": "%d hücre seçildi"
+               },
+               "columns": {
+                   "1": "1 sütun seçildi",
+                   "_": "%d sütun seçildi"
+               }
+           },
+           "autoFill": {
+               "cancel": "İptal",
+               "fillHorizontal": "Hücreleri yatay olarak doldur",
+               "fillVertical": "Hücreleri dikey olarak doldur",
+               "fill": "Bütün hücreleri <i>%d<\/i> ile doldur"
+           },
+           "buttons": {
+               "collection": "Koleksiyon <span class=\"ui-button-icon-primary ui-icon ui-icon-triangle-1-s\"><\/span>",
+               "colvis": "Sütun görünürlüğü",
+               "colvisRestore": "Görünürlüğü eski haline getir",
+               "copySuccess": {
+                   "1": "1 satır panoya kopyalandı",
+                   "_": "%ds satır panoya kopyalandı"
+               },
+               "copyTitle": "Panoya kopyala",
+               "csv": "CSV",
+               "excel": "Excel",
+               "pageLength": {
+                   "-1": "Bütün satırları göster",
+                   "_": "%d satır göster"
+               },
+               "pdf": "PDF",
+               "print": "Yazdır",
+               "copy": "Kopyala",
+               "copyKeys": "Tablodaki veriyi kopyalamak için CTRL veya u2318 + C tuşlarına basınız. İptal etmek için bu mesaja tıklayın veya escape tuşuna basın."
+           },
+           "searchBuilder": {
+               "add": "Koşul Ekle",
+               "button": {
+                   "0": "Arama Oluşturucu",
+                   "_": "Arama Oluşturucu (%d)"
+               },
+               "condition": "Koşul",
+               "conditions": {
+                   "date": {
+                       "after": "Sonra",
+                       "before": "Önce",
+                       "between": "Arasında",
+                       "empty": "Boş",
+                       "equals": "Eşittir",
+                       "not": "Değildir",
+                       "notBetween": "Dışında",
+                       "notEmpty": "Dolu"
+                   },
+                   "number": {
+                       "between": "Arasında",
+                       "empty": "Boş",
+                       "equals": "Eşittir",
+                       "gt": "Büyüktür",
+                       "gte": "Büyük eşittir",
+                       "lt": "Küçüktür",
+                       "lte": "Küçük eşittir",
+                       "not": "Değildir",
+                       "notBetween": "Dışında",
+                       "notEmpty": "Dolu"
+                   },
+                   "string": {
+                       "contains": "İçerir",
+                       "empty": "Boş",
+                       "endsWith": "İle biter",
+                       "equals": "Eşittir",
+                       "not": "Değildir",
+                       "notEmpty": "Dolu",
+                       "startsWith": "İle başlar"
+                   },
+                   "array": {
+                       "contains": "İçerir",
+                       "empty": "Boş",
+                       "equals": "Eşittir",
+                       "not": "Değildir",
+                       "notEmpty": "Dolu",
+                       "without": "Hariç"
+                   }
+               },
+               "data": "Veri",
+               "deleteTitle": "Filtreleme kuralını silin",
+               "leftTitle": "Kriteri dışarı çıkart",
+               "logicAnd": "ve",
+               "logicOr": "veya",
+               "rightTitle": "Kriteri içeri al",
+               "title": {
+                   "0": "Arama Oluşturucu",
+                   "_": "Arama Oluşturucu (%d)"
+               },
+               "value": "Değer",
+               "clearAll": "Filtreleri Temizle"
+           },
+           "searchPanes": {
+               "clearMessage": "Hepsini Temizle",
+               "collapse": {
+                   "0": "Arama Bölmesi",
+                   "_": "Arama Bölmesi (%d)"
+               },
+               "count": "{total}",
+               "countFiltered": "{shown}\/{total}",
+               "emptyPanes": "Arama Bölmesi yok",
+               "loadMessage": "Arama Bölmeleri yükleniyor ...",
+               "title": "Etkin filtreler - %d"
+           },
+           "thousands": ".",
+           "datetime": {
+               "amPm": [
+                   "öö",
+                   "ös"
+               ],
+               "hours": "Saat",
+               "minutes": "Dakika",
+               "next": "Sonraki",
+               "previous": "Önceki",
+               "seconds": "Saniye",
+               "unknown": "Bilinmeyen",
+               "weekdays": {
+                   "6": "Paz",
+                   "5": "Cmt",
+                   "4": "Cum",
+                   "3": "Per",
+                   "2": "Çar",
+                   "1": "Sal",
+                   "0": "Pzt"
+               },
+               "months": {
+                   "9": "Ekim",
+                   "8": "Eylül",
+                   "7": "Ağustos",
+                   "6": "Temmuz",
+                   "5": "Haziran",
+                   "4": "Mayıs",
+                   "3": "Nisan",
+                   "2": "Mart",
+                   "11": "Aralık",
+                   "10": "Kasım",
+                   "1": "Şubat",
+                   "0": "Ocak"
+               }
+           },
+           "decimal": ",",
+           "editor": {
+               "close": "Kapat",
+               "create": {
+                   "button": "Yeni",
+                   "submit": "Kaydet",
+                   "title": "Yeni kayıt oluştur"
+               },
+               "edit": {
+                   "button": "Düzenle",
+                   "submit": "Güncelle",
+                   "title": "Kaydı düzenle"
+               },
+               "error": {
+                   "system": "Bir sistem hatası oluştu (Ayrıntılı bilgi)"
+               },
+               "multi": {
+                   "info": "Seçili kayıtlar bu alanda farklı değerler içeriyor. Seçili kayıtların hepsinde bu alana aynı değeri atamak için buraya tıklayın; aksi halde her kayıt bu alanda kendi değerini koruyacak.",
+                   "noMulti": "Bu alan bir grup olarak değil ancak tekil olarak düzenlenebilir.",
+                   "restore": "Değişiklikleri geri al",
+                   "title": "Çoklu değer"
+               },
+               "remove": {
+                   "button": "Sil",
+                   "confirm": {
+                       "_": "%d adet kaydı silmek istediğinize emin misiniz?",
+                       "1": "Bu kaydı silmek istediğinizden emin misiniz?"
+                   },
+                   "submit": "Sil",
+                   "title": "Kayıtları sil"
+               }
+           }
+       }
     });
 
     /* DataTables end here */
@@ -336,4 +508,79 @@
             });
 
     });
+
+    $(function () {
+        const url = '/Admin/Role/Assign/';
+        const placeHolderDiv = $('#modalPlaceHolder');
+        $(document).on('click',
+            '.btn-assign',
+            function (event) {
+                event.preventDefault();
+                const id = $(this).attr('data-id');
+                $.get(url, { userId: id }).done(function (data) {
+                    placeHolderDiv.html(data);
+                    placeHolderDiv.find('.modal').modal('show');
+                }).fail(function (err) {
+                    toastr.error(`${err.responseText}`, 'Hata!');
+                });
+            });
+
+        /* Ajax POST / Updating a Comment starts from here */
+
+        placeHolderDiv.on('click',
+            '#btnUpdate',
+            function (event) {
+                event.preventDefault();
+                const form = $('#form-comment-update');
+                const actionUrl = form.attr('action');
+                const dataToSend = new FormData(form.get(0));
+                $.ajax({
+                    url: actionUrl,
+                    type: 'POST',
+                    data: dataToSend,
+                    processData: false,
+                    contentType: false,
+                    success: function (data) {
+                        const commentUpdateAjaxModel = jQuery.parseJSON(data);
+                        console.log(commentUpdateAjaxModel);
+                        const newFormBody = $('.modal-body', commentUpdateAjaxModel.CommentUpdatePartial);
+                        placeHolderDiv.find('.modal-body').replaceWith(newFormBody);
+                        const isValid = newFormBody.find('[name="IsValid"]').val() === 'True';
+                        if (isValid) {
+                            const id = commentUpdateAjaxModel.CommentDto.Comment.Id;
+                            const tableRow = $(`[name="${id}"]`);
+                            placeHolderDiv.find('.modal').modal('hide');
+                            dataTable.row(tableRow).data([
+                                commentUpdateAjaxModel.CommentDto.Comment.Id,
+                                commentUpdateAjaxModel.CommentDto.Comment.Article.Title,
+                                commentUpdateAjaxModel.CommentDto.Comment.Text.length > 75 ? commentUpdateAjaxModel.CommentDto.Comment.Text.substring(0, 75) : commentUpdateAjaxModel.CommentDto.Comment.Text,
+                                `${commentUpdateAjaxModel.CommentDto.Comment.IsActive ? "Evet" : "Hayır"}`,
+                                `${commentUpdateAjaxModel.CommentDto.Comment.IsDeleted ? "Evet" : "Hayır"}`,
+                                `${convertToShortDate(commentUpdateAjaxModel.CommentDto.Comment.CreatedDate)}`,
+                                commentUpdateAjaxModel.CommentDto.Comment.CreatedByName,
+                                `${convertToShortDate(commentUpdateAjaxModel.CommentDto.Comment.ModifiedDate)}`,
+                                commentUpdateAjaxModel.CommentDto.Comment.ModifiedByName,
+                                getButtonsForDataTable(commentUpdateAjaxModel.CommentDto.Comment)
+                            ]);
+                            tableRow.attr("name", `${id}`);
+                            dataTable.row(tableRow).invalidate();
+                            toastr.success(`${commentUpdateAjaxModel.CommentDto.Comment.Id} no'lu yorum başarıyla güncellenmiştir`, "Başarılı İşlem!");
+                        } else {
+                            let summaryText = "";
+                            $('#validation-summary > ul > li').each(function () {
+                                let text = $(this).text();
+                                summaryText = `*${text}\n`;
+                            });
+                            toastr.warning(summaryText);
+                        }
+                    },
+                    error: function (error) {
+                        console.log(error);
+                        toastr.error(`${err.responseText}`, 'Hata!');
+                    }
+                });
+            });
+
+    });
+
 });
