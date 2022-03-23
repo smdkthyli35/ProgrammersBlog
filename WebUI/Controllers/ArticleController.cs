3b7cc81f592dcc4d78e2f73aa9052a01a76889b7
zeljkosamed <smdkthyli@gmail.com>
2022-03-23 23:34:14 +0300

    ArticleController ve Detail action'ı oluşturuldu.

---
+++ WebUI/Controllers/ArticleController.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int articleId)
+        {
+            var articleResult = await _articleService.GetAsync(articleId);
+            if (articleResult.ResultStatus == ResultStatus.Success)
+            {
+                return View(articleResult.Data);
+            }
+
+            return NotFound();
+        }
+    }
+}
