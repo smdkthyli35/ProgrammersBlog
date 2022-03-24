2040defb52e3f5aeae97f72ad68e22c4b33540bb
zeljkosamed <smdkthyli@gmail.com>
2022-03-24 21:49:56 +0300

    ArticleService, Manager ve Home Controller Index paging'e göre düzenlendi.

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -17,11 +17,11 @@ namespace WebUI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index(int? categoryId)
+        public async Task<IActionResult> Index(int? categoryId, int currentPage = 1, int pageSize = 5)
         {
             var articlesResult = await (categoryId == null
-                ? _articleService.GetAllByNonDeletedAndActiveAsync()
-                : _articleService.GetAllByCategoryAsync(categoryId.Value));
+                ? _articleService.GetAllByPagingAsync(null, currentPage, pageSize)
+                : _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize));
             return View(articlesResult.Data);
         }
     }
