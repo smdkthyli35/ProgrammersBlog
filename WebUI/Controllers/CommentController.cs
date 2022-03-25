ab3bb335b336ac3ddd18f0aafb8ae79dc7e86fbf
zeljkosamed <smdkthyli@gmail.com>
2022-03-25 22:48:33 +0300

    Yorum ekleme işleminin backend kısmı oluşturuldu.

---
+++ WebUI/Controllers/CommentController.cs
@@ -0,0 +1,53 @@
+﻿using Core.Utilities.Extensions;
+using Core.Utilities.Results.ComplexTypes;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Add(CommentAddDto commentAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _commentService.AddAsync(commentAddDto);
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var commentAddAjaxViewModel = JsonSerializer.Serialize(new CommentAddAjaxViewModel
+                    {
+                        CommentDto = result.Data,
+                        CommentAddPartial = await this.RenderViewToStringAsync("_CommentAddPartial", commentAddDto)
+                    }, new JsonSerializerOptions
+                    {
+                        ReferenceHandler = ReferenceHandler.Preserve
+                    });
+                    return Json(commentAddAjaxViewModel);
+                }
+                ModelState.AddModelError("", result.Message);
+            }
+            var commentAddAjaxErrorViewModel = JsonSerializer.Serialize(new CommentAddAjaxViewModel
+            {
+                CommentAddDto = commentAddDto,
+                CommentAddPartial = await this.RenderViewToStringAsync("_CommentAddPartial", commentAddDto)
+            });
+            return Json(commentAddAjaxErrorViewModel);
+        }
+    }
+}
---
+++ WebUI/Models/CommentAddAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class CommentAddAjaxViewModel
+    {
+        public CommentAddDto CommentAddDto { get; set; }
+        public string CommentAddPartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
