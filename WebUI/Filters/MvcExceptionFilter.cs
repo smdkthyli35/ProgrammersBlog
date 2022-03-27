5fb14fd0a3f0775fd44a6dfd2b08df192bcb11bc
zeljkosamed <smdkthyli@gmail.com>
2022-03-27 23:58:25 +0300

    NLog kurulum, konfigürasyon ve loglama işlemleri gerçekleştirildi.

--- WebUI/Filters/MvcExceptionFilter.cs
+++ WebUI/Filters/MvcExceptionFilter.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
@@ -16,11 +17,13 @@ namespace WebUI.Filters
     {
         private readonly IHostEnvironment _environment;
         private readonly IModelMetadataProvider _metadataProvider;
+        private readonly ILogger _logger;
 
-        public MvcExceptionFilter(IHostEnvironment environment, IModelMetadataProvider metadataProvider)
+        public MvcExceptionFilter(IHostEnvironment environment, IModelMetadataProvider metadataProvider, ILogger<MvcExceptionFilter> logger)
         {
             _environment = environment;
             _metadataProvider = metadataProvider;
+            _logger = logger;
         }
 
         public void OnException(ExceptionContext context)
@@ -37,17 +40,20 @@ namespace WebUI.Filters
                         mvcErrorModel.Detail = context.Exception.Message;
                         result = new ViewResult { ViewName = "Error" };
                         result.StatusCode = 500;
+                        _logger.LogError(context.Exception, context.Exception.Message);
                         break;
                     case NullReferenceException:
                         mvcErrorModel.Message = "Üzgünüz, işleminiz sırasında beklenmedik bir null veriye rastlandı. Sorunu en kısa sürede çözeceğiz.";
                         mvcErrorModel.Detail = context.Exception.Message;
                         result = new ViewResult { ViewName = "Error" };
                         result.StatusCode = 403;
+                        _logger.LogError(context.Exception, context.Exception.Message);
                         break;
                     default:
                         mvcErrorModel.Message = "Üzgünüz, işleminiz sırasında beklenmedik bir hata oluştu. Sorunu en kısa sürede çözeceğiz.";
                         result = new ViewResult { ViewName = "Error" };
                         result.StatusCode = 500;
+                        _logger.LogError(context.Exception, "Bu benim log hata mesajım!");
                         break;
                 }
                 result.ViewData = new ViewDataDictionary(_metadataProvider, context.ModelState);
