e9ed04f5e2ac4b65134db949586bf913efaf6979
zeljkosamed <smdkthyli@gmail.com>
2022-03-30 22:20:43 +0300

    Options controller ile about action'ı oluşturuldu.

---
+++ WebUI/Areas/Admin/Controllers/OptionsController.cs
@@ -0,0 +1,57 @@
+﻿using Core.Utilities.Helpers.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NToastNotify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OptionsController : Controller
+    {
+        private readonly AboutUsPageInfo _aboutUsPageInfo;
+        private readonly IWritableOptions<AboutUsPageInfo> _aboutUsPageInfoWriter;
+        private readonly IToastNotification _toastNotification;
+
+        public OptionsController(IOptionsSnapshot<AboutUsPageInfo> aboutUsPageInfo, IWritableOptions<AboutUsPageInfo> aboutUsPageInfoWriter, IToastNotification toastNotification)
+        {
+            _aboutUsPageInfo = aboutUsPageInfo.Value;
+            _aboutUsPageInfoWriter = aboutUsPageInfoWriter;
+            _toastNotification = toastNotification;
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View(_aboutUsPageInfo);
+        }
+
+        [HttpPost]
+        public IActionResult About(AboutUsPageInfo aboutUsPageInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _aboutUsPageInfoWriter.Update(x =>
+                {
+                    x.Header = aboutUsPageInfo.Header;
+                    x.Content = aboutUsPageInfo.Content;
+                    x.SeoAuthor = aboutUsPageInfo.SeoAuthor;
+                    x.SeoDescription = aboutUsPageInfo.SeoDescription;
+                    x.SeoTags = aboutUsPageInfo.SeoTags;
+                });
+                _toastNotification.AddSuccessToastMessage("Hakkımızda sayfa içerikleri başarıyla güncellenmiştir.", new ToastrOptions
+                {
+                    Title = "Başarılı İşlem!"
+                });
+                return View(aboutUsPageInfo);
+            }
+            return View(_aboutUsPageInfo);
+        }
+    }
+}
