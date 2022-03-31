3ed5aaf3b9c4224eefa31f2e177f7a30b648ed36
zeljkosamed <smdkthyli@gmail.com>
2022-03-31 18:52:49 +0300

    EfEntityRepositoryBase için GetAsyncV2 ve GetAllAsyncV2 metodları eklendi.

--- Core/DataAccess/Abstract/IEntityRepository.cs
+++ Core/DataAccess/Abstract/IEntityRepository.cs
@@ -11,7 +11,12 @@ namespace Core.DataAccess.Abstract
     public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        Task<T> GetAsyncV2(IList<Expression<Func<T, bool>>> predicates, IList<Expression<Func<T, object>>> includeProperties);
+        //if(isActive == true) predicates.Add()
+
         Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+        Task<IList<T>> GetAllAsyncV2(IList<Expression<Func<T, bool>>> predicates, IList<Expression<Func<T, object>>> includeProperties);
+
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task DeleteAsync(T entity);
