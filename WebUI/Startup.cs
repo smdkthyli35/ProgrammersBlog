d52d1f975f72cf5100cc9db3b7a7bd5e2312a9fd
zeljkosamed <smdkthyli@gmail.com>
2022-03-31 16:58:23 +0300

    ArticleRightSideBarWidgetOptionsViewModel oluşturuldu. ArticleRightSideBarWidgetOptions json içerisine eklendi.

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -35,11 +35,12 @@ namespace WebUI
             services.Configure<AboutUsPageInfo>(Configuration.GetSection("AboutUsPageInfo"));
             services.Configure<WebsiteInfo>(Configuration.GetSection("WebsiteInfo"));
             services.Configure<SmtpSettings>(Configuration.GetSection("SmtpSettings"));
+            services.Configure<ArticleRightSideBarWidgetOptions>(Configuration.GetSection("ArticleRightSideBarWidgetOptions"));
 
             services.ConfigureWritable<AboutUsPageInfo>(Configuration.GetSection("AboutUsPageInfo"));
             services.ConfigureWritable<WebsiteInfo>(Configuration.GetSection("WebsiteInfo"));
             services.ConfigureWritable<SmtpSettings>(Configuration.GetSection("SmtpSettings"));
-
+            services.ConfigureWritable<ArticleRightSideBarWidgetOptions>(Configuration.GetSection("ArticleRightSideBarWidgetOptions"));
 
             services.AddControllersWithViews(options =>
             {
