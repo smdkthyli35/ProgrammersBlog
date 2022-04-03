ebdacc112d2d1c9b6e9be2a42d338876cc23af1e
zeljkosamed <smdkthyli@gmail.com>
2022-04-04 01:35:00 +0300

    Chart.js ile makalelerin okunma ve yorum sayısı görüntülendi.

--- WebUI/wwwroot/AdminLTE/js/homeIndex.js
+++ WebUI/wwwroot/AdminLTE/js/homeIndex.js
@@ -211,58 +211,45 @@
 
     // Chart.js
 
-    const categories = [
-        {
-            name: 'C#',
-            viewCount: '24500'
-        },
-        {
-            name: 'C++',
-            viewCount: '77000'
-        },
-        {
-            name: 'Javascript',
-            viewCount: '57800'
-        },
-        {
-            name: 'Dart',
-            viewCount: '2250'
-        },
-        {
-            name: 'PHP',
-            viewCount: '123000'
-        },
-        {
-            name: 'Typescript',
-            viewCount: '18999'
-        }
-    ];
+    $.get('/Admin/Article/GetAllByViewCount/?isAscending=false&takeSize=10',
+        function (data) {
+            const articleResult = jQuery.parseJSON(data);
 
-
-    let viewCountContext = $('#viewCountChart');
-    let viewCountChart = new Chart(viewCountContext,
-        {
-            type: 'bar',
-            data: {
-                labels: categories.map(category => category.name),
-                datasets: [{
-                    label: 'Okunma Sayısı',
-                    data: categories.map(category => category.viewCount),
-                    backgroundColor: ['#0E185F', '#FD5D5D', '#85C88A', '#333C83', '#733C3C'],
-                    hoverBorderWidth: 4,
-                    hoverBorderColor: 'black'
-                }]
-            },
-            options: {
-                plugins: {
-                    legend: {
-                        labels: {
-                            font: {
-                                size: 18
+            let viewCountContext = $('#viewCountChart');
+            let viewCountChart = new Chart(viewCountContext,
+                {
+                    type: 'bar',
+                    data: {
+                        labels: articleResult.$values.map(article => article.Title),
+                        datasets: [
+                            {
+                                label: 'Okunma Sayısı',
+                                data: articleResult.$values.map(article => article.ViewsCount),
+                                backgroundColor: '#fb3640',
+                                hoverBorderWidth: 4,
+                                hoverBorderColor: 'black'
+                            },
+                            {
+                                label: 'Yorum Sayısı',
+                                data: articleResult.$values.map(article => article.CommentCount),
+                                backgroundColor: '#fdca40',
+                                hoverBorderWidth: 4,
+                                hoverBorderColor: 'black'
+                            }]
+                    },
+                    options: {
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    font: {
+                                        size: 18
+                                    }
+                                }
                             }
                         }
                     }
-                }
-            }
-        });
+                });
+        })
+
+
 });
